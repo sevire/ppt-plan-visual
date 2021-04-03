a321d5188ce92242b664c2a6d03f490ed2677f9f
Thomas (LI) <tbg-livestockinformation@genonline.co.uk>
2021-04-03 17:34:26 +0100

    #177616269 Placing of visual elements on the visual takes account of swimlanes
    Interim commit - not fully working code

--- source/plan_visual_driver.py
+++ source/plan_visual_driver.py
@@ -21,9 +21,15 @@ class PlanVisualiser:
     """
 
     def __init__(self, plan_data, plot_config, format_config, template_path):
+        # The actual plan data with activities and milestones, start/finish dates etc.
         self.plan_data = plan_data
+
+        # Data to define plot area for elements, tracks etc.
         self.plot_config = plot_config
+
+        # Data with pre-determined formatting properties to apply to elements.
         self.format_config = format_config
+
         self.template = template_path
 
         folder, base, ext = get_path_name_ext(template_path)
@@ -35,6 +41,8 @@ class PlanVisualiser:
         self.shapes = visual_slide.shapes
         self.plot_driver = PlotDriver(plot_area_config)
 
+        self.swimlane_driver = self.extract_swimlane_data()
+
     @classmethod
     def from_excel(cls, plan_data_excel_path, excel_driver_config, template_path):
         excel_manager = ExcelPlan(excel_driver_config, plan_data_excel_path)
@@ -44,6 +52,68 @@ class PlanVisualiser:
         extracted_plan_data = excel_manager.read_plan_data()
 
         return PlanVisualiser(extracted_plan_data, extracted_plot_config, extracted_format_config, template_path)
+
+    def extract_swimlane_data(self):
+        """
+        After some thought am taking a very simple approach here.
+
+        Just go through each activity on the plan and:
+        - Check which swimlane it is for
+        - Look at the track number and height in tracks and therefore work out the bottom track number
+        - If the bottom track number is larger than that already recorded, update to reflect.
+        - Once you have checked every activity, the entry in each swimlane will have recorded the number of tracks
+          required for that swimlane.
+        - We can then go back and calculate the start and end track for each swimlane which is what the plot method
+          needs.
+
+          So we will end up with a dict, with one entry for each (named) swimlane, and against each swimlane we will
+          see the start track number and the end track number.
+
+        :return:
+        """
+
+        swimlane_data = {}
+        entry_num = 0
+        for record in self.plan_data:
+            swimlane = record['swimlane']
+            track_num_low = record['track_num']
+            track_num_high = record['track_num'] + record['bar_height_in_tracks'] - 1
+            if swimlane not in swimlane_data:
+                # Adding record for this swimlane. Also remember ordering as is important later.
+                swimlane_record = {
+                    'swimlane_order': entry_num + 1,
+                    'lowest_track_within_lane': track_num_low,
+                    'highest_track_within_lane': track_num_high
+                }
+                swimlane_data[swimlane] = swimlane_record
+                entry_num += 1
+            else:
+                swimlane_record = swimlane_data[swimlane]
+                if track_num_low < swimlane_record['lowest_track_within_lane']:
+                    swimlane_record['lowest_track_within_lane'] = track_num_low
+                if track_num_high > swimlane_record['highest_track_within_lane']:
+                    swimlane_record['highest_track_within_lane'] = track_num_high
+
+        # We now have a dict containing a record for each swimlane of lowest and highest relative track number used.
+        # Can now calculate the start and end track number for each swimlane - in order that lanes were encountered.
+
+        swimlane_plot_data = {}
+        end_track = 0
+        for lane_number in range(0, len(swimlane_data)):
+            swimlane_entries = [(key, swimlane_data[key]) for key in swimlane_data.keys() if swimlane_data[key]['swimlane_order'] == lane_number + 1]
+
+            assert(len(swimlane_entries) == 1)
+
+            swimlane, swimlane_entry = swimlane_entries[0]
+            tracks_for_swimlane = \
+                swimlane_entry['highest_track_within_lane'] - swimlane_entry['lowest_track_within_lane'] + 1
+            start_track = end_track + 1
+            end_track = end_track + tracks_for_swimlane - 1
+            swimlane_plot_data[swimlane] = {
+                'start_track': start_track,
+                'end_track': end_track
+            }
+        return swimlane_plot_data
 
     def plot_slide(self):
         """
@@ -61,6 +131,7 @@ class PlanVisualiser:
             if plotable_element['type'] == 'bar':
                 start = plotable_element['start_date']
                 end = plotable_element['end_date']
+                swimlane = plotable_element['swimlane']
                 track_num = plotable_element['track_num']
                 num_tracks = plotable_element['bar_height_in_tracks']
                 shape_format = plotable_element['format_properties']
@@ -68,15 +139,17 @@ class PlanVisualiser:
                 # start_date = self.plot_driver.string_date_to_date(start)
                 # end_date = self.plot_driver.string_date_to_date(end)
 
-                self.plot_bar(start, end, track_num, num_tracks, shape_format)
+                self.plot_bar(start, end, swimlane, track_num, num_tracks, shape_format)
 
         self.prs.save(self.slides_out_path)
 
-    def plot_bar(self, start_date, end_date, track_number, num_tracks, format):
+    def plot_bar(self, start_date, end_date, swimlane, track_number, num_tracks, format):
+        swimlane_start = self.swimlane_driver[swimlane]['start_track']
+
         left = self.plot_driver.date_to_x_coordinate(start_date)
         right = self.plot_driver.date_to_x_coordinate(end_date)
 
-        top = self.plot_driver.track_number_to_y_coordinate(track_number)
+        top = self.plot_driver.track_number_to_y_coordinate(swimlane_start + track_number - 1) - 1
         bottom = top + self.plot_driver.height_of_track(num_tracks)
 
         shape = self.shapes.add_shape(
