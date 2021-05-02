c268eea3ecdd5a9ef727e781bb567dbc9971ffdb
Thomas (LI) <tbg-livestockinformation@genonline.co.uk>
2021-05-02 10:23:22 +0100

    Added dummy test to help with build process

--- source/tests/test_ppt_plan.py
+++ source/tests/test_ppt_plan.py
@@ -3,4 +3,6 @@ from unittest import TestCase
 
 class TestApp(TestCase):
     def test_dummy(self):
+        # For now just using this to drive the testing bit of the github action.
+        # Will replace with a proper test at some point :-)
         self.assertTrue(True)
