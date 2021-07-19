7ace46186216eed1355cd388b8127daead58ea12
seivre <tbg-github@genonline.co.uk>
2021-07-19 14:20:29 +0100

    #178834872 Text alignment not working after re-factor

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ import pathlib
 from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
-VERSION = '0.0.10'
+VERSION = '0.0.17'
 
 INSTALL_REQUIRES = [
       'numpy',
       'pandas',
-      'python-pptx'
-]
+      'python-pptx',
+      'dataclasses',
+      'colour',
+      'ddt==1.4.2',
+      'openpyxl',
+      ]
 
 setup(
       version=VERSION,
       install_requires=INSTALL_REQUIRES,
-      packages=find_packages()
       )
