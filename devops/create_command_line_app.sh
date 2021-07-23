5d39fe84d6b3e2b90593fac26eff065298005c8a
seivre <tbg-github@genonline.co.uk>
2021-07-23 18:48:39 +0100

    Getting app build working again after last few changes.

--- devops/create_command_line_app.sh
+++ devops/create_command_line_app.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Remove pre-existing build and dist folders in case there was a problem in previous run
-rm -rf build
-rm -rf dist
+rm -rf app_build
+rm -rf app_dist
 
 pyinstaller source/ppt_plot_plan_main.py --onefile
-mv dist/ppt_plot_plan_main .
+mv app_dist/ppt_plot_plan_main .
 
 ls -l ppt_plot_plan_main
 
 # Remove build and dist folders after build and move
-rm -rf build
-rm -rf dist
+rm -rf app_build
+rm -rf app_dist
