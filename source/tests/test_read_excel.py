1f36c8bbf43a159df3816b7a6ce5710a1cc51097
seivre <tbg-github@genonline.co.uk>
2021-08-02 08:32:49 +0100

    Updated to synchronise python versions so that all package versions are correct in requirements.txt.

--- source/tests/test_read_excel.py
+++ source/tests/test_read_excel.py
@@ -5,7 +5,7 @@ from source.visualiser.read_excel import read_excel
 
 class TestReadExcel(TestCase):
     def test_read_excel(self):
-        excel_path = '/Users/Development/PycharmProjects/ppt-plan-visual/source/tests/test_resources/input_files/config_files/KBT-VisualConfig.xlsx'
+        excel_path = 'test_resources/input_files/config_files/KBT-VisualConfig.xlsx'
         sheet_name = 'FormatConfig'
         table = read_excel(excel_path, sheet_name)
 
